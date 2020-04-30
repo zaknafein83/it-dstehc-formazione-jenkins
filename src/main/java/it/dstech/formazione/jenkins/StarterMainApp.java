bb9e414836098058b9f34a7527bab9664428e50a
Francesco Sisca <franksisca@gmail.com>
2020-04-30 12:47:21 +0200

    aggiunti primo file e gitignore

---
+++ src/main/java/it/dstech/formazione/jenkins/StarterMainApp.java
@@ -0,0 +1,13 @@
+package it.dstech.formazione.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterMainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterMainApp.class, args);
+	}
+
+}
